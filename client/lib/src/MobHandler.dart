9b00af8311616ded22ab8f0cd62f1da4b47132b0
jaketroxel@gmail.com <jtroxel@bertrand.(none)>
2014-02-17 18:16:55 -0800

    Added skeleton of MobHandler.dart

---
+++ client/lib/src/MobHandler.dart
@@ -0,0 +1,17 @@
+part of terd;
+
+class MobHandler
+{
+  // Uses a list to hold all the mobs.
+  List<Mob> _objects;
+  
+  MobHandler(){}
+  
+  void addMob(Mob mob){}
+  
+  void deleteMob(int index){}
+  
+  void update(){}
+  
+  void render(){}
+}
